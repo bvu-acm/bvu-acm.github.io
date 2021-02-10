78adfb08acabaf27bd9e591aeb88ebac62ac955c
gtystahl <gtystahl@gmail.com>
2021-02-10 16:19:20 -0600

    This adds a method of quickly forming the dictionary as well as a basic outline for the commandline and main function

--- AlexaProject/main.py
+++ AlexaProject/main.py
@@ -1,1 +1,17 @@
+from commandprompt import *
+import Person
+
 print("hello world")
+
+person = Person.Person()
+display(person.location)
+
+while True:
+    inp = input("Enter a command: ")
+
+    if inp == "exit":
+        break
+    else:
+        command(inp, person)
+
+print("Goodbye")
