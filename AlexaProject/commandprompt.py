c68b1c4f99ccf4131a1dc7415176ad6fd621dd32
HullJac <jacobhull55@gmail.com>
2021-02-10 21:27:16 -0600

    Adding change to command prompt from meeting.

--- AlexaProject/commandprompt.py
+++ AlexaProject/commandprompt.py
@@ -1,22 +1,38 @@
 import places
 
-
 def command(commandString, person):
     # command string holds the string of the given command and person is the person object that holds
     # the players info
 
     # For ide debugging purposes
     # commandString = ""
+    commandString = commandString.lower()
+    commandStringLst = commandString.split()
 
-    # I am putting this in a variables because it will allow us to change it later if we want the words
-    # to be different
-    com1 = "go to"
-    com2 = "display"
-    if com1 in commandString:
-        place = commandString[len(com1) + 1:]
-        goto(place, person)
-    elif com2 in commandString:
-        display(person.location)
+    loneCommands = ["help", "hunt", "save", "run away", "sleep"]
+    singleCommands = ["take", "eat", "drink"]
+    doubleCommands = [["talk", "to"], ["attack", "with"], ["trade", "with"], ["go", "to"]]
+
+    if commandString in loneCommands:
+        if commandString == "help":
+            displayHelp()
+
+        if commandString == "hunt":
+
+        if commandString == "save":
+
+        if commandString == "run away":
+
+        if commandString == "sleep":
+
+
+    elif commandStringlst[0] in singleCommands:
+
+    elif commandStringlst[:1] in doubleCommands:
+        
+    else:
+        print("That is not a valid command.")
+        print('If you need help, type "help"')
 
 
 def goto(place, person):
@@ -28,3 +44,9 @@ def goto(place, person):
 def display(location):
     print(places.descriptions[location])
     print(places.connections[location])
+
+def displayHelp():
+    print("The available commands are:")
+    print("\thunt\n\tsave\n\trun away\n\tsleep\n\ttake 'item'\n\teat 'item'")
+    print("\tdrink 'item'\n\ttalk to 'person'\n\tattack with 'weapon'")
+    print("\ttrade with 'person'\n\tgo to 'place'")
