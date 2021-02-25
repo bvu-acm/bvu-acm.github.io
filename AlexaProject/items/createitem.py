94f553ef491658153a7f8508bc254858b11a6bf0
gtystahl <gtystahl@gmail.com>
2021-02-24 19:55:11 -0600

    This is a start that will be worked on later

---
+++ AlexaProject/items/createitem.py
@@ -0,0 +1,16 @@
+# import weopon
+# import food
+# etc
+
+def createItem(name):
+
+    weopons = ["sword"]
+    food = ["apple"]
+
+    # if name in weopons:
+    #   make weopon object
+    # elif name in food:
+    #   make food object
+    # etc
+
+    # return item object
